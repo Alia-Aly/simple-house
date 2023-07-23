0dc93043819ab1611555f52a5af61749da49526e
Alia-Aly <123838431+Alia-Aly@users.noreply.github.com>
2023-07-23 16:44:35 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+function nodel() {
+    document.getElementById("pizza").style.display ="none";
+    document.getElementById("pizza1").style.display ="none";
+    document.getElementById("pizza2").style.display ="none";
+    document.getElementById("salad").style.display ="block";
+    document.getElementById("salad1").style.display ="block";
+    document.getElementById("btn-11").style.backgroundColor = "#fff";
+    document.getElementById("btn-11").style.color = "#2F956D";
+    document.getElementById("btn-12").style.backgroundColor = "#fff";
+    document.getElementById("btn-12").style.color = "#2F956D";
+    document.getElementById("btn-13").style.backgroundColor = "#2F956D";
+    document.getElementById("btn-13").style.color = "#fff";
+}
+function piza(){
+    document.getElementById("pizza").style.display ="block";
+    document.getElementById("pizza1").style.display ="block";
+    document.getElementById("pizza2").style.display ="block";
+    document.getElementById("salad").style.display ="block";
+    document.getElementById("salad1").style.display ="block";
+    document.getElementById("btn-11").style.backgroundColor = "#2F956D";
+    document.getElementById("btn-11").style.color = "#fff";
+    document.getElementById("btn-12").style.backgroundColor = "#fff";
+    document.getElementById("btn-12").style.color = "#2F956D";
+    document.getElementById("btn-13").style.backgroundColor = "#fff";
+    document.getElementById("btn-13").style.color = "#2F956D";
+}
+function salada(){
+    document.getElementById("pizza1").style.display ="none";
+    document.getElementById("salad").style.display ="none";
+    document.getElementById("salad1").style.display ="none";
+    document.getElementById("pizza").style.display ="block";
+    document.getElementById("pizza2").style.display ="block";
+    document.getElementById("btn-11").style.backgroundColor = "#fff";
+    document.getElementById("btn-11").style.color = "#2F956D";
+    document.getElementById("btn-12").style.backgroundColor = "#2F956D";
+    document.getElementById("btn-12").style.color = "#fff";
+    document.getElementById("btn-13").style.backgroundColor = "#fff";
+    document.getElementById("btn-13").style.color = "#2F956D";
+
+}
+// ===========================================================================================================
